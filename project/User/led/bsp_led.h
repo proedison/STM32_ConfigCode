473bfd96bcd4a9354cb0e56e26d09f0881a0db40
edison <edisonhesih@126.com>
2019-05-09 11:36:53 +0800

    Signed-off-by: edison <edisonhesih@126.com>

--- project/User/led/bsp_led.h
+++ project/User/led/bsp_led.h
@@ -62,55 +62,6 @@
 #define LED3_OFF		   digitalHi(LED3_GPIO_PORT,LED3_GPIO_PIN)
 #define LED3_ON			   digitalLo(LED3_GPIO_PORT,LED3_GPIO_PIN)
 
-/* 基本混色，后面高级用法使用PWM可混出全彩颜色,且效果更好 */
-
-//红
-#define LED_RED  \
-					LED1_ON;\
-					LED2_OFF\
-					LED3_OFF
-
-//绿
-#define LED_GREEN		\
-					LED1_OFF;\
-					LED2_ON\
-					LED3_OFF
-
-//蓝
-#define LED_BLUE	\
-					LED1_OFF;\
-					LED2_OFF\
-					LED3_ON
-
-					
-//黄(红+绿)					
-#define LED_YELLOW	\
-					LED1_ON;\
-					LED2_ON\
-					LED3_OFF
-//紫(红+蓝)
-#define LED_PURPLE	\
-					LED1_ON;\
-					LED2_OFF\
-					LED3_ON
-
-//青(绿+蓝)
-#define LED_CYAN \
-					LED1_OFF;\
-					LED2_ON\
-					LED3_ON
-					
-//白(红+绿+蓝)
-#define LED_WHITE	\
-					LED1_ON;\
-					LED2_ON\
-					LED3_ON
-					
-//黑(全部关闭)
-#define LED_RGBOFF	\
-					LED1_OFF;\
-					LED2_OFF\
-					LED3_OFF
 
 void LED_GPIO_Config(void);
 
